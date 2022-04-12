27cee3bc563fa4017b710d92425afba5e4240eb4
Warre002 <warre.bossaert@gmail.com>
2022-04-12 13:46:26 +0200

    Firebase

--- src/index.js
+++ src/index.js
@@ -8,6 +8,21 @@ import {
   Route
 } from "react-router-dom";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBO_ET0TGdrVmEe5AN7JFHhz-okNvSTrxw",
+  authDomain: "drippy-elephants.firebaseapp.com",
+  projectId: "drippy-elephants",
+  storageBucket: "drippy-elephants.appspot.com",
+  messagingSenderId: "40370301170",
+  appId: "1:40370301170:web:107aedf440d080ae84c86b",
+  measurementId: "G-F5957J8W07"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
     <BrowserRouter>
